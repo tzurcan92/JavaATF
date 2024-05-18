3748218818af74cbbc163920d9576c46b3b2e17f
sergiu <tzurcan92@gmail.com>
2024-05-18 16:50:14 +0300

    Added the implementation for the DriverManager

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,7 +1,24 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
+import javax.swing.*;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        String currentTabName = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver");
+        driver.close();
+
+        driver.switchTo().window(currentTabName);
+        driver.get("https://apple.stackexchange.com/questions/338898/how-to-copy-path-of-a-file-in-mac-os");
+        driver.quit();
+
+        System.out.println("The test is finished and driver is closed");
     }
 }
