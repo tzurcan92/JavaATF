25ed034ee6154cdd8bf36ed41f378b72d7747b4e
sergiu <tzurcan92@gmail.com>
2024-05-24 16:57:17 +0300

    Added the implementation for the DriverManager

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,8 +1,9 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DataGeneratorManager;
 import online.tekwillacademy.managers.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import online.tekwillacademy.managers.ScrollManager;
+import org.openqa.selenium.*;
 
 import javax.swing.*;
 
@@ -12,11 +13,56 @@ public class TestRunner {
         WebDriver driver = DriverManager.getInstance().getDriver();
         String currentTabName = driver.getWindowHandle();
         driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver");
+        driver.get("https://www.tekwillacademy-opencart.online/");
+        System.out.println("The current url is: "+driver.getCurrentUrl());
+        System.out.println("The page title is: "+driver.getTitle());
+        WebElement userDropDownIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        userDropDownIcon.click();
+
+        WebElement registredOption = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+        registredOption.click();
+        System.out.println("The current url is: "+driver.getCurrentUrl());
+        System.out.println("The page title is: "+driver.getTitle());
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys("Gabriel");
+
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys("Turcan");
+
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        String emailData = DataGeneratorManager.getRandomEmail();
+        emailInput.sendKeys(emailData);
+        System.out.println("Email: +" +emailInput);
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys("12345678!");
+
+        WebElement siteAgreement = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+        ScrollManager.scrollToElement(siteAgreement);siteAgreement.click();
+
+
+
+        WebElement continueButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        continueButton.click();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("The current url is: "+driver.getCurrentUrl());
+        System.out.println("The page title is: "+driver.getTitle());
+
+
+
+
+        /*driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver");*/
         driver.close();
 
         driver.switchTo().window(currentTabName);
-        driver.get("https://apple.stackexchange.com/questions/338898/how-to-copy-path-of-a-file-in-mac-os");
+        driver.get("https://www.tekwillacademy-opencart.online/");
         driver.quit();
 
         System.out.println("The test is finished and driver is closed");
